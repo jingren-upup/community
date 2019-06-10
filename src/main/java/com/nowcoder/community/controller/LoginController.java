8b2d272a88832ce3729ec245fb9f0b33db36ab16
lihonghe <lihonghe@nowcoder.com>
2019-06-10 21:37:16 +0800

    注册

---
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -0,0 +1,63 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+@Controller
+public class LoginController implements CommunityConstant {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = "/register", method = RequestMethod.GET)
+    public String getRegisterPage() {
+        return "/site/register";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String getLoginPage() {
+        return "/site/login";
+    }
+
+    @RequestMapping(path = "/register", method = RequestMethod.POST)
+    public String register(Model model, User user) {
+        Map<String, Object> map = userService.register(user);
+        if (map == null || map.isEmpty()) {
+            model.addAttribute("msg", "注册成功,我们已经向您的邮箱发送了一封激活邮件,请尽快激活!");
+            model.addAttribute("target", "/index");
+            return "/site/operate-result";
+        } else {
+            model.addAttribute("usernameMsg", map.get("usernameMsg"));
+            model.addAttribute("passwordMsg", map.get("passwordMsg"));
+            model.addAttribute("emailMsg", map.get("emailMsg"));
+            return "/site/register";
+        }
+    }
+
+    // http://localhost:8080/community/activation/101/code
+    @RequestMapping(path = "/activation/{userId}/{code}", method = RequestMethod.GET)
+    public String activation(Model model, @PathVariable("userId") int userId, @PathVariable("code") String code) {
+        int result = userService.activation(userId, code);
+        if (result == ACTIVATION_SUCCESS) {
+            model.addAttribute("msg", "激活成功,您的账号已经可以正常使用了!");
+            model.addAttribute("target", "/login");
+        } else if (result == ACTIVATION_REPEAT) {
+            model.addAttribute("msg", "无效操作,该账号已经激活过了!");
+            model.addAttribute("target", "/index");
+        } else {
+            model.addAttribute("msg", "激活失败,您提供的激活码不正确!");
+            model.addAttribute("target", "/index");
+        }
+        return "/site/operate-result";
+    }
+
+}
---
+++ src/main/java/com/nowcoder/community/util/CommunityUtil.java
@@ -0,0 +1,25 @@
+package com.nowcoder.community.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.DigestUtils;
+
+import java.util.UUID;
+
+public class CommunityUtil {
+
+    // 生成随机字符串
+    public static String generateUUID() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    // MD5加密
+    // hello -> abc123def456
+    // hello + 3e4a8 -> abc123def456abc
+    public static String md5(String key) {
+        if (StringUtils.isBlank(key)) {
+            return null;
+        }
+        return DigestUtils.md5DigestAsHex(key.getBytes());
+    }
+
+}
