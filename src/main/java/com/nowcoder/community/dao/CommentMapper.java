a6ece82415c705f07e9ca4afdde3e03402ab3e29
wangjingren <434582431@qq.com>
2021-05-08 19:01:01 +0800

    评论区 设置

---
+++ src/main/java/com/nowcoder/community/dao/CommentMapper.java
@@ -0,0 +1,17 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+
+    List<Comment> selectCommentByEntity(int entityType, int entityId , int offset, int limit);
+
+    int selectCountByEntity(int entityType, int entityId);
+
+
+
+}
