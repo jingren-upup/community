dde0b0fa8ab864ab594c326161edaad095124cdb
wangjingren <434582431@qq.com>
2021-05-09 12:22:42 +0800

    显示评论

--- src/main/java/com/nowcoder/community/controller/DiscussPostContoller.java
+++ src/main/java/com/nowcoder/community/controller/DiscussPostContoller.java
@@ -1,9 +1,13 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.entity.Comment;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.CommentService;
 import com.nowcoder.community.service.DiscussPostService;
 import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +15,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/discuss")
-public class DiscussPostContoller {
+public class DiscussPostContoller implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
@@ -25,6 +32,9 @@ public class DiscussPostContoller {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private CommentService commentService;
 
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
@@ -46,7 +56,7 @@ public class DiscussPostContoller {
     }
 
     @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
-    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model){
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model, Page page){
         //帖子
         DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
         model.addAttribute("post",post);
@@ -54,6 +64,14 @@ public class DiscussPostContoller {
         User user = userService.findUserById(post.getUserId());
         model.addAttribute("user",user);
 
+        //评论的分页信息
+        page.setLimit(5);
+        page.setPath("/discuss/detail/" + discussPostId);
+        page.setRows(post.getCommentCount());
+
+        List<Comment> commentList = commentService .findCommentsByEntity(
+                ENTITY_TYPE_POST,post.getId(),page.getOffset(),page.getLimit());
+        List<Map<String,Object>> commentVoList =  new ArrayList<>();
         return "/site/discuss-detail";
     }
 
