2ca55d531b1b44d9da8d14c94601f8f4cb577b5f
wangjingren <434582431@qq.com>
2021-05-10 20:55:51 +0800

    评论全显

--- src/main/java/com/nowcoder/community/controller/DiscussPostContoller.java
+++ src/main/java/com/nowcoder/community/controller/DiscussPostContoller.java
@@ -15,10 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 @RequestMapping("/discuss")
@@ -69,9 +66,55 @@ public class DiscussPostContoller implements CommunityConstant {
         page.setPath("/discuss/detail/" + discussPostId);
         page.setRows(post.getCommentCount());
 
+        // 评论：给帖子的评论
+        // 回复：给评论的评论
+        // 评论列表
         List<Comment> commentList = commentService .findCommentsByEntity(
                 ENTITY_TYPE_POST,post.getId(),page.getOffset(),page.getLimit());
+        // 评论的VO列表
         List<Map<String,Object>> commentVoList =  new ArrayList<>();
+        if(commentList != null){
+
+            for (Comment comment : commentList){
+                // 评论VO
+                Map<String,Object> commentVo = new HashMap<>();
+                // 评论
+                commentVo.put("comment",comment);
+                // 作者
+                commentVo.put("user",userService.findUserById(comment.getUserId()));
+
+                // 回复列表
+                List<Comment> replyList = commentService.findCommentsByEntity(
+                         ENTITY_TYPE_COMMENT,comment.getId(),0,Integer.MAX_VALUE);
+
+                // 回复VO列表
+                List<Map<String,Object>> replyVoList = new ArrayList<>();
+                 if (replyList!=null){
+                     for( Comment reply :replyList){
+                            Map<String,Object> replyVo = new HashMap<>();
+                            // 回复
+                            replyVo.put("reply",reply);
+                            // 作者
+                            replyVo.put("user",userService.findUserById(reply.getUserId()));
+                            // 回复目标
+                            User target = reply.getTargetId() == 0 ? null : userService.findUserById(reply.getTargetId());
+                            replyVo.put("target",target);
+
+                            replyVoList.add(replyVo);
+                     }
+                     commentVo.put("replys",replyVoList);
+
+                     // 回复数量
+                     int replyCount = commentService.findCommentCount(ENTITY_TYPE_COMMENT,comment.getId());
+                     commentVo.put("replyCount",replyCount);
+
+                     commentVoList.add(commentVo);
+
+                 }
+
+            }
+        }
+        model.addAttribute("comments",commentVoList);
         return "/site/discuss-detail";
     }
 
